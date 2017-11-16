a7aecda98294ddcc61655af4d6f36e29b4c085fb
Patrick Gund <patrick.gund@gmail.com>
2017-11-16 11:28:30 -0600

    feat: wireframe for user homepage

--- client/components/AllActivities.js
+++ client/components/AllActivities.js
@@ -94,12 +94,22 @@ class AllActivites extends React.Component {
                 <Container width={11}>
                   <Segment>
                      <Header size='large'>First Activity</Header>
-                     <Grid>
+                     <Grid columns={2}>
                       <Grid.Column>
                        <Image src='square-image.png' size='medium' rounded />
                        </Grid.Column>
                        <Grid.Column>
-                         
+                         <div>
+                           <Header size='small'>Duration: </Header> 7.5 min
+                         </div>
+                         <br /><br /><br /><br />
+                         <div>
+                           <Header size='small'>Pace: </Header> 5.00 min/mile
+                         </div>
+                         <br /><br /><br /><br />
+                         <div>
+                           <Header size='small'>Miles: </Header> 1.5 miles
+                         </div>
                        </Grid.Column>
 
                     </Grid>
