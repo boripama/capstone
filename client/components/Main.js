fdc31fa341ddff19b7cb6b3cf30d157f24ef078f
Matthew Thor <mjthor@gmail.com>
2017-11-16 17:05:33 -0600

    cleaned up copy-pasted code

--- client/components/Main.js
+++ client/components/Main.js
@@ -16,26 +16,7 @@ const Main = (props) => {
 
   return (
     <div>
-<<<<<<< HEAD:client/components/main.js
-      <h1>ANANDA</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
-=======
       <Navbar />
->>>>>>> master:client/components/Main.js
       <hr />
       {children}
     </div>
