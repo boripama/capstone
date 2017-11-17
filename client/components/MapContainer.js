f6f981f333c1788e4dbd0aeb41f708ceaffc2f9b
Richard Polidoro <armyofbear136@gmail.com>
2017-11-17 17:11:02 -0600

    modified polyline object and map generation

--- client/components/MapContainer.js
+++ client/components/MapContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-import { Polyline } from './index';
+// import { Polyline } from './index';
 
 
 export class MapContainer extends Component {
@@ -15,18 +15,33 @@ export class MapContainer extends Component {
       ]
     };
   }
+
   componentDidMount() {
     const routeReturn = window.google.maps.geometry.encoding.decodePath(this.props.polyline);
+    const pLine = new window.google.maps.Polyline({
+      path: this.state.tempCoords,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+    // let map = new window.google.maps.Map(document.getElementById('map'), {
+    //   zoom: 14,
+    //   center: { lat: 34.366, lng: -89.519 }
+    // }); //standard google maps map object
     console.log('route coordinates: ', routeReturn);
-    this.setState({ routeCoords: routeReturn });
+    this.setState({ routeCoords: routeReturn, line: pLine });
 
   }
+
   render() {
+
 
     if (this.state.routeCoords.length) {
       console.log('I AM THE ROUTE TO BE RENDERED: ', this.state.tempCoords);
       return (
         <div>
+
 
           <Map
             google={this.props.google}
@@ -35,8 +50,12 @@ export class MapContainer extends Component {
               lng: -87.635384 //fullstack
             }}
             zoom={14}
+            onGoogleApiLoaded={({ map, maps }) => {
+              this.setState({ map: map, maps: maps, mapLoaded: true });
+              this.state.line.setMap(map);
+            }}
           >
-            <Polyline path={this.state.tempCoords} />
+
 
             <Marker
               onClick={this.onMarkerClick}
@@ -57,4 +76,5 @@ export default GoogleApiWrapper({
   apiKey: ('YAIzaSyCcF35bbRWUQbTvP4t7XkY62MS5JDJ_oZk')
 })(MapContainer);
 
-/* onGoogleApiLoaded={({ map, maps }) => { this.setState({ map: map, maps: maps, mapLoaded: true }); }} */
+
+// this.line = new window.google.maps.Polyline(Object.assign({}, renderedPolyline, this.props.path));
