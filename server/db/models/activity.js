cb2a86c3362a9b977a02fa6c387585581924502b
Matthew Thor <mjthor@gmail.com>
2017-11-19 18:05:53 -0600

    add center point to activity model and calculate automatically

--- server/db/models/activity.js
+++ server/db/models/activity.js
@@ -32,6 +32,9 @@ const Activity = db.define('activity', {
   durationMs: {
     type: Sequelize.INTEGER,
   },
+  center: {
+    type: Sequelize.ARRAY(Sequelize.FLOAT)
+  },
   durationTimestamp: { // readable timestamp for display purposes
     type: Sequelize.VIRTUAL,
     get () {
@@ -47,6 +50,14 @@ Activity.prototype.decodePoly = function() {
   return convertPolylineToPoints(this.polyline);
 };
 
+Activity.prototype.setCenter = function(pointsArray) {
+  const lng = pointsArray.reduce((total, point) => total + point[0], 0) / pointsArray.length;
+  const lat = pointsArray.reduce((total, point) => total + point[1], 0) / pointsArray.length;
+
+  this.centerLng = lng;
+  this.centerLat = lat;
+};
+
 /**
  * classMethods
  */
@@ -54,12 +65,20 @@ Activity.prototype.decodePoly = function() {
 /**
  * hooks
  */
+
+// set duration and center
 Activity.beforeSave((activity, options) => {
   const start = activity.startTime;
   const end = activity.endTime;
   activity.durationMs = getDuration(end, start);
+
+  const points = activity.decodePoly(activity.polyline);
+  const lng = points.reduce((tot, p) => tot + p[0], 0) / points.length;
+  const lat = points.reduce((tot, p) => tot + p[1], 0) / points.length;
+  activity.center = [lng, lat];
 });
 
+// updates user totals after activity is updated
 Activity.afterUpdate(async (activity, options) => {
   const user = await User.findById(activity.userId);
   user.updateTotals(activity);
