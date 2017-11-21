f043bedbfdf42f43e71b9aca038e2fb8646794be
Bond Davis <bonddavis92@gmail.com>
2017-11-21 11:01:54 -0600

    Hide navbar optinos when logged out

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -29,24 +29,29 @@ const Navbar = (props) => {
       style={{ padding: '0em 0em' }}
       vertical
     >
-      <Container>
-        <Menu inverted pointing secondary size="large">
-          <Menu.Item as={Link} to="/home">Home</Menu.Item>
-          <Menu.Item as={Link} to="/activity/1">Activity 1</Menu.Item>
+      
           {
             isLoggedIn ?
-              <Menu.Item as={Link} onClick={handleClick} to="/login">Logout</Menu.Item>
+              <Container>
+                <Menu inverted pointing secondary size="large">
+                  <Menu.Item as={Link} to="/home">Home</Menu.Item>
+                  <Menu.Item as={Link} to="/activity/1">Activity 1</Menu.Item>
+                  <Menu.Item as={Link} to="/uploadActivity">Upload Activity</Menu.Item>
+                  <Menu.Item as={Link} to="/activities">Activities</Menu.Item>
+                  <Menu.Item as={Link} onClick={handleClick} to="/login">Logout</Menu.Item>
+                  <Menu.Item as={Link} to="/user">Account</Menu.Item>
+                </Menu>
+              </Container>
+              
               :
               <Container>
+              <Menu inverted pointing secondary size="large">
                 <Menu.Item as={Link} to="/login">Login</Menu.Item>
                 <Menu.Item as={Link} to="/signup">Sign Up</Menu.Item>
+              </Menu>
               </Container>
           }
-          <Menu.Item as={Link} to="/uploadActivity">Upload Activity</Menu.Item>
-          <Menu.Item as={Link} to="/activities">Activities</Menu.Item>
-          <Menu.Item as={Link} to="/user">Account</Menu.Item>
-        </Menu>
-      </Container>
+
       <Divider inverted />
     </Segment>
   );
