e05fe0319bcacb3340b712f7483509c90b9dc37d
richardspolidoro <armyofbear136@gmail.com>
2017-11-27 17:33:56 -0600

    spelling and divs

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -28,80 +28,76 @@ class UserProfile extends Component {
     const { activities, user, followers, selectedUser, suggested } = this.props;
     if (activities[0] && selectedUser.id && followers[0]) {
       return (
-        <div>
-          <Grid centered columns={2}>
-            <Grid.Column width={11}>
-              <Grid.Row>
-                <FollowerGroup suggested={suggested} />
-              </Grid.Row>
-              <Grid.Row>
-                <br />
-                <Container width={11}>
-                  {
-                    activities.slice(-10).map(activity => {
-                      return (
-                        <ActivityContainer key={activity.id} activity={activity}>{activity.title}</ActivityContainer>
-                      );
-                    })
-                  }
-                </Container>
-              </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Grid.Row>
-                <ProfileDescription user={selectedUser} />
-              </Grid.Row>
-              <Grid.Row>
-                <br />
-                <br />
-                <p>Followers: </p>
+        <Grid centered columns={2}>
+          <Grid.Column width={11}>
+            <Grid.Row>
+              <FollowerGroup suggested={suggested} />
+            </Grid.Row>
+            <Grid.Row>
+              <br />
+              <Container width={11}>
                 {
-                  followers.map(follower => {
-                    return <div key={follower.id}>
-                      <small>
-                        <Link to={`/profile/${follower.id}`}>{follower.name}</Link>
-                      </small>
-                    </div>
+                  activities.slice(-10).map(activity => {
+                    return (
+                      <ActivityContainer key={activity.id} activity={activity}>{activity.title}</ActivityContainer>
+                    );
                   })
                 }
-              </Grid.Row>
-            </Grid.Column>
-          </Grid>
-        </div>
+              </Container>
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Grid.Row>
+              <ProfileDescription user={selectedUser} />
+            </Grid.Row>
+            <Grid.Row>
+              <br />
+              <br />
+              <p>Followers: </p>
+              {
+                followers.map(follower => {
+                  return <div key={follower.id}>
+                    <small>
+                      <Link to={`/profile/${follower.id}`}>{follower.name}</Link>
+                    </small>
+                  </div>
+                })
+              }
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
       );
     }
     else if (selectedUser.id) {
       return (
-        <div>
-          <Grid centered columns={2}>
-            <Grid.Column width={11}>
-              <Grid.Row>
-                <FollowerGroup suggested={suggested} />
-              </Grid.Row>
-              <Grid.Row>
-                <br />
-                <Container width={11}>
-                  {selectedUser.id === user.id
-                    ? <h1>
-                      Yo, runner... go upload some activites!
+        <Grid centered columns={2}>
+          <Grid.Column width={11}>
+            <Grid.Row>
+              <FollowerGroup suggested={suggested} />
+            </Grid.Row>
+            <Grid.Row>
+              <br />
+              <Container width={11}>
+                {selectedUser.id === user.id
+                  ? <h1>
+                    Yo, runner... go upload some activities!
                         <br />
-                      <NewActivity />
-                    </h1>
-                    : <div>
-                      <h1>
-                        {selectedUser.name} needs some activities! Send them some
+                    <NewActivity />
+                  </h1>
+                  : <div>
+                    <h1>
+                      {selectedUser.name} needs some activities! Send them some
                           <a href={'mailto:' + selectedUser.email}> encouragement</a>.
                         </h1>
-                    </div>
-                  }
-                </Container>
-              </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <ProfileDescription user={selectedUser} />
-            </Grid.Column>
-          </Grid>
-        </div >
+                  </div>
+                }
+              </Container>
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <ProfileDescription user={selectedUser} />
+          </Grid.Column>
+        </Grid>
       );
     }
     else { return null; }
