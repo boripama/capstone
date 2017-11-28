28b75e0e38d273ebce22dea42fce2b8f1b2e8c99
Patrick Gund <patrick.gund@gmail.com>
2017-11-28 15:03:13 -0600

    feat: completed button to view next or previous ten activities

--- client/components/AllActivities.js
+++ client/components/AllActivities.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   Container,
   Grid,
+  Button,
 } from 'semantic-ui-react';
 import { ProfileCard, FollowerGroup, ActivityContainer } from './index';
 import { connect } from 'react-redux';
@@ -11,20 +12,37 @@ import { fetchActivities, fetchSuggested } from '../store';
 class AllActivities extends Component {
   constructor() {
     super();
+    this.page = 0;
   }
+   state = { page: 0 };
 
-  componentDidMount() {
-    this.props.fetchData(this.props.user.id);
+
+   componentDidMount() {
+     this.props.fetchData(this.props.user.id);
+   }
+
+  changePageForward = () => {
+    this.page = this.page + 1;
+    this.setState({page: this.page });
+  }
+  changePageBack = () => {
+    if (this.page) this.page = this.page - 1;
+    this.setState({page: this.page });
   }
 
   render() {
     const {activities, suggested} = this.props;
     if (activities[0]) {
+      let startVal = activities.length - ((this.state.page + 1) * 10);
+      let endVal = activities.length - (this.state.page * 10);
+      let tenActivities = activities.slice(startVal, endVal);
       return (
         <div>
           <Grid centered columns={2}>
             <Grid.Column width={3}>
               <ProfileCard />
+              <Button style={{ margin: '1em 0em' }} onClick={this.changePageForward}>Next Ten Activities </Button>
+              <Button style={{ margin: '1em 0em' }} onClick={this.changePageBack}>Previous Ten Activities </Button>
             </Grid.Column>
             <Grid.Column width={11}>
               <Grid.Row>
@@ -34,7 +52,7 @@ class AllActivities extends Component {
                 <br />
                 <Container width={11}>
                   {
-                    activities.slice(-10).map(activity => {
+                    tenActivities.map(activity => {
                       return (
                         <ActivityContainer key={activity.id} activity={activity}>{activity.title}</ActivityContainer>
                       );
