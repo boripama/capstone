c1df91bb0df1f6b13a17df513d093f3f40dd0015
Bond Davis <bonddavis92@gmail.com>
2017-11-28 17:28:03 -0600

    added footer and different sections

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -8,28 +8,25 @@ class LandingPage extends Component {
 
   render() {
     return (
-      <div>
-        <header className="header">
-          <div className="header__logo-box">
-            
+      <div className="landingPageBody">
+        <header className="header1">
+          <div className="header1__logo-box">       
           </div>
-          <div className="header__text-box">
+          <div className="header1__text-box">
             <h1 className="heading-primary">
               <span className="heading-primary--main">Ready to run?</span>
               <span className="heading-primary--sub">Let's get started!</span>
             </h1>
-
             <a href="#" className="btn btn--white btn--animated">Find out more</a>
           </div>
-
         </header>
 
 
         <main>
           <section className="section-about">
-            <div className="u-center-text u-margin-bottom-big">
+            <div className="u-center-text u-margin-top-big u-margin-bottom-big">
               <h2 className="heading-secondary">
-                The ultimate site for your athletes!
+                The ultimate site for athletes!
                     </h2>
             </div>
 
@@ -75,8 +72,8 @@ class LandingPage extends Component {
                   <figcaption className="story__caption">Mary Smith</figcaption>
                 </figure>
                 <div className="story__text">
-                  <div className="h3 heading-tertiary u-margin-bottom-small">I had the best week ever with my family</div>
-                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus autem id minima, Necessitatibus autem id minima, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus autem id minima, Necessitatibus autem id minima, tatibus autem id minima.</p>
+                  <div className="h3 heading-tertiary u-margin-bottom-small">And I ran! I ran so far away!</div>
+                  <p>Been using Ananda for the last 10 weeks. It's kept me really motivated and I get to keep up with all of my runner friends</p>
 
                 </div>
               </div>
@@ -88,8 +85,8 @@ class LandingPage extends Component {
                   <figcaption className="story__caption">Jack Wilson</figcaption>
                 </figure>
                 <div className="story__text">
-                  <div className="h3 heading-tertiary u-margin-bottom-small">WOW! My life is completely different now</div>
-                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus autem id minima, Necessitatibus autem id minima, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus autem id minima, Necessitatibus autem id minima, tatibus autem id minima.</p>
+                  <div className="h3 heading-tertiary u-margin-bottom-small">My favorite social media site!</div>
+                  <p>Couldn't be more happy so far. I've gotten in much better shape competing daily against my good buddies Will and Phil.</p>
 
                 </div>
               </div>
@@ -101,6 +98,30 @@ class LandingPage extends Component {
           </section>
 
         </main>
+
+
+        <footer className="footer">
+          <div className="footer__logo-box">
+            </div>
+            <div className="row">
+              <div className="col-1-of-2">
+                <div className="footer__navigation">
+                  <ul className="footer__list">
+                    <li className="footer__item"><a href="#" className="footer__link">Company</a></li>
+                    <li className="footer__item"><a href="#" className="footer__link">Contact us</a></li>
+                    <li className="footer__item"><a href="#" className="footer__link">Careers</a></li>
+                    <li className="footer__item"><a href="#" className="footer__link">Privacy policy</a></li>
+                    <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-1-of-2">
+                <p className="footer_copyright">
+                  Built by <a href="#" className="footer__link">Bond Davis, Pattrick Gund, Rick Polidoro, Matthew Thor</a>
+                </p>
+              </div>
+            </div>
+        </footer>
       </div>
 
     )
